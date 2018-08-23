62fbfd5a7a62a52cfd686f69c0bc03fbd7cb0e8a
MichaelLangbein <michael_langbein@hotmail.de>
2018-08-23 22:06:53 +0200

    finally agreed upon a numbering scheme. in -> W0 -> x0 -> y0 -> W1 -> x1 -> y1 -> W2 -> x2 -> y2. L = 2 = len(inputs)-2

--- prototype/test.py
+++ prototype/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def outer(vecA, vecB):
@@ -13,15 +14,16 @@ def inner(matrix, vec):
 class NN:
 
     def __init__(self, layerSizes):
-        self.L = len(layerSizes)
-        self.x = [0] * self.L
-        self.W = [0] * self.L
-        self.y = [0] * self.L
-        self.dEdx = [0] * self.L
-        self.dEdW = [0] * self.L
-        for l in range(1, self.L):
-            f = layerSizes[l-1]
-            t = layerSizes[l]
+        nrLayers = len(layerSizes) - 1
+        self.L = nrLayers - 1 # we count from 0
+        self.x = [0] * nrLayers
+        self.W = [0] * nrLayers
+        self.y = [0] * nrLayers
+        self.dEdx = [0] * nrLayers
+        self.dEdW = [0] * nrLayers
+        for l in range(0, self.L+1): # range is exclusive last value
+            f = layerSizes[l]
+            t = layerSizes[l+1]
             self.W[l] =  np.random.rand(t, f)
 
 
@@ -31,37 +33,39 @@ class NN:
 
 
     def diffAct(self, x):
-        return np.exp(x) / (1. + np.exp(x))**2
+        return np.exp(-x) / (1. + np.exp(-x))**2
 
 
     def predict(self, inpt):
         # 1.1.  First Layer
-        self.y[0] = inpt
-        
+        self.x[0] = inner( self.W[0], inpt)
+        self.y[0] = self.actFct(self.x[0])
+
         # 1.2.  Higher layers
-        for l in range(1, self.L):
+        for l in range(1, self.L+1): # range is exclusive last value
             self.x[l] = inner( self.W[l], self.y[l-1] )
             self.y[l] = self.actFct(self.x[l])
         
-        return self.y[self.L - 1]
+        return self.y[self.L]
 
 
     def backprop(self, target):
         # 2.1.  Top Layer
-        self.dEdx[self.L-1] = (target - self.y[self.L-1]) * self.diffAct(self.x[self.L-1])
-        self.dEdW[self.L-1] = outer( self.dEdx[self.L-1], self.y[self.L-1])
+        self.dEdx[self.L] = (target - self.y[self.L]) * self.diffAct(self.x[self.L])
+        self.dEdW[self.L] = outer( self.dEdx[self.L], self.y[self.L-1])
 
         # 2.2.  Lower layers
-        for l in range(self.L-1, 1):
+        for l in range(self.L-1, -1, -1): # range is exclusive last value
             self.dEdx[l] = self.dEdx[l+1] * self.W[l+1] * self.diffAct(self.x[l])
             self.dEdW[l] = outer( self.dEdx[l], self.y[l-1] )
         
         return self.dEdW
 
         
-    def training(self, inputs, targets):
+    def training(self, inputs, targets, decay):
 
         alpha = 1
+        offBy = []
 
         for s in range(len(inputs)):
             print "now training on {}th sample with alpha = {}".format(s, alpha)
@@ -72,25 +76,30 @@ class NN:
             output = self.predict(inpt)
             dEdW = self.backprop(target)
 
-            for l in range(self.L):
+            for l in range(self.L + 1): # range is eclusive last value
                 self.W[l] -= alpha * dEdW[l]
             
-            alpha *= 0.99
+            alpha *= decay
 
             print "Training result: should be {}, is {}".format(target, output)
+            offBy.append(abs(target - output))
+        
+        plt.plot(offBy)
+        plt.show()
+
 
 
 
 
 inputs = []
 targets = []
-for i in range(1000):
+for i in range(10000):
     a = np.random.randint(2)
     b = np.random.randint(2)
     o = [(a+b)%2]
     inputs.append([a, b])
     targets.append(o)
 
-nn = NN([2, 3, 4, 3, 1])
-nn.training(inputs, targets)
+nn = NN([2, 5, 10, 10, 4, 1])
+nn.training(inputs, targets, 0.999)
 print nn.predict([1,0])
