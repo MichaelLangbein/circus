57826a01421485c2f905f006140c10b90036a619
Michael Langbein <michael_langbein@hotmail.de>
2018-08-28 13:05:21 +0000

    Update test.py

--- prototype/test.py
+++ prototype/test.py
@@ -41,7 +41,7 @@ class NN:
 
     def predict(self, inpt):
         # 1.1.  First Layer
-        self.x[0] = inner(self.W[0], inpt) + self.b[0]
+        self.x[0] = inner( self.W[0], inpt ) + self.b[0]
         self.y[0] = self.actFct(self.x[0])
 
         # 1.2.  Higher layers
@@ -55,12 +55,12 @@ class NN:
     def backprop(self, target):
         # 2.1.  Top Layer
         self.dEdx[self.L] = (target - self.y[self.L]) * self.diffAct(self.x[self.L])
-        self.dEdW[self.L] = outer( self.dEdx[self.L], self.y[self.L-1])
+        self.dEdW[self.L] = outer( self.dEdx[self.L], self.y[self.L-1] )
         self.dEdb[self.L] = self.dEdx[self.L]
 
         # 2.2.  Lower layers
         for l in range(self.L-1, -1, -1): # range is exclusive last value
-            self.dEdx[l] = inner(self.dEdx[l+1], self.W[l+1]) * self.diffAct(self.x[l])
+            self.dEdx[l] = inner( np.transpose(self.W[l+1]), self.dEdx[l+1] ) * self.diffAct(self.x[l])
             self.dEdW[l] = outer( self.dEdx[l], self.y[l-1] )
             self.dEdb[l] = self.dEdx[l]
         
